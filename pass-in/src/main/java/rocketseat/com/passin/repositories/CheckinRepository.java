b9a4f7fc583959cf16e6cc5c80dae459f56695e8
Vvatte <vvatte@yahoo.com>
2024-05-23 17:46:01 -0300

    aula 2

--- pass-in/src/main/java/rocketseat/com/passin/repositories/CheckinRepository.java
+++ pass-in/src/main/java/rocketseat/com/passin/repositories/CheckinRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.checkin.CheckIn;
 import rocketseat.com.passin.domain.event.Event;
 
+import java.util.Optional;
+
 public interface CheckinRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
